2fc8eb291c5677718fc9f631e54700eaba18a429
duydvtomosia <duydinh@tomosia.com>
2023-02-02 20:16:14 +0700

    init project

---
+++ EditVideo/AdjustTypeCell/AdjustTypeCell.swift
@@ -0,0 +1,18 @@
+//
+//  AdjustTypeCell.swift
+//  EditVideo
+//
+//  Created by tomosia on 02/02/2023.
+//
+
+import FSPagerView
+import UIKit
+
+class AdjustTypeCell: FSPagerViewCell {
+    @IBOutlet var contentImageView: UIImageView!
+    @IBOutlet var isEditedView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- EditVideo/ViewController.swift
+++ EditVideo/ViewController.swift
@@ -5,24 +5,14 @@
 //  Created by tomosia on 31/01/2023.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import CRRulerControl
 import FSPagerView
 import UIKit
+import YPImagePicker
 
-enum EditType: CaseIterable {
+enum EditType: String, CaseIterable {
     case adjust
     case filter
-
-    var image: UIImage {
-        switch self {
-        case .filter:
-            return UIImage(named: "ic_filter")!
-        case .adjust:
-            return UIImage(named: "ic_adjust")!
-        }
-    }
 }
 
 enum FilterType: CaseIterable {
@@ -58,54 +48,111 @@ enum FilterType: CaseIterable {
     }
 }
 
+enum AdjustType: CaseIterable {
+    case exposure
+    case highlights
+    case shadows
+    case contrast
+    case brightness
+    case saturation
+
+    var name: String {
+        switch self {
+        case .exposure: return "Exposure"
+        case .saturation: return "Saturation"
+        case .brightness: return "Brightness"
+        case .contrast: return "Contrast"
+        case .shadows: return "Shadows"
+        case .highlights: return "Highlights"
+        }
+    }
+
+    var image: String {
+        switch self {
+        case .exposure: return "ic_exposure"
+        case .saturation: return "ic_saturation"
+        case .brightness: return "ic_brightness"
+        case .contrast: return "ic_contrast"
+        case .shadows: return "ic_shadows"
+        case .highlights: return "ic_highlights"
+        }
+    }
+}
+
 class ViewController: UIViewController {
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var rulerControlView: CRRulerControl!
     @IBOutlet var cancelButton: UIButton!
     @IBOutlet var doneButton: UIButton!
-    @IBOutlet var popUpButton: UIButton!
     @IBOutlet var editTypeImageView: UIImageView!
-    @IBOutlet var previewFSPagerView: FSPagerView!
+    @IBOutlet var filterPagerView: FSPagerView!
+    @IBOutlet var adjustPagerView: FSPagerView!
+    @IBOutlet var attributeNameTextField: UITextField!
 
-    let editTypes: [EditType] = EditType.allCases
-    let listFilter: [FilterType] = FilterType.allCases
-    let image = UIImage(named: "Image")
-    var currentFilter: String? {
+    var editType: EditType = EditType.adjust {
         didSet {
-            guard let ciImage = CIImage(image: image ?? UIImage()),
-                  let filteredImage = filterImage(ciImage, intensity: currentIntensity)
+            titleTextField.text = editType.rawValue.uppercased()
+            adjustPagerView.isHidden = editType == .filter
+        }
+    }
+
+    let listFilter: [FilterType] = FilterType.allCases
+    let listAdjust: [AdjustType] = AdjustType.allCases
+    var image = UIImage() {
+        didSet {
+            attributeNameTextField.isHidden = image == UIImage()
+            attributeNameTextField.text = editType == .filter ? currentFilter : currentAdjust?.name
+            imageView.image = image
+            filterPagerView.reloadData()
+        }
+    }
+
+    var currentFilter = FilterType.original.identifier {
+        didSet {
+            guard let ciImage = CIImage(image: image),
+                  let filteredImage = filterImage(ciImage,
+                                                  filter: currentFilter)?.resize(to: CGSize(width: 720,
+                                                                                            height: 720)),
+                  let attribute = listFilter.first(where: { filterType in
+                      currentFilter == filterType.identifier
+                  }).map({ filter in
+                      filter.name
+                  })
             else { return }
+            attributeNameTextField.text = attribute
             imageView.image = filteredImage
         }
     }
 
-    var currentIntensity: Double = 1.0
+    var currentAdjust: AdjustType? {
+        didSet {
+            guard let type = currentAdjust else { return }
+            attributeNameTextField.text = type.name
+            switch type {
+            case .contrast, .brightness, .saturation:
+                AdjustImage.shared.colorControl(type: type, input: rulerControlView.value)
+                break
+            default: break
+            }
+        }
+    }
+
     let context = CIContext(options: nil)
+    let imageSize = CGSize(width: 80.0, height: 80.0)
+    var imagePickerView = YPImagePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupRulerControl()
-
-        imageView.image = image
-
-        popUpButton.menu = UIMenu(
-            options: .displayInline,
-            children: listFilter.map({ filter in
-                UIAction(title: filter.name, handler: handle)
-            }))
-    }
-
-    @objc func handle(_ action: UIAction) {
-        currentFilter = listFilter.first(where: { filterType in
-            filterType.name == action.title
-        }).map({ filterType in
-            filterType.identifier
-        })
+        setupPagerView()
+        configImagePickerView()
+        attributeNameTextField.isHidden = true
     }
 
     func setupRulerControl() {
+        rulerControlView.isHidden = editType == .filter
         rulerControlView.rangeFrom = 0
         rulerControlView.rangeLength = 100
         rulerControlView.setFrequency(25, for: .major)
@@ -114,12 +161,35 @@ class ViewController: UIViewController {
         rulerControlView.value = 100
     }
 
-    func filterImage(_ input: CIImage, intensity: Double) -> UIImage? {
-        if currentFilter != FilterType.original.identifier {
-            let filter = CIFilter(name: currentFilter ?? "")
+    func setupPagerView() {
+        filterPagerView.delegate = self
+        filterPagerView.dataSource = self
+        filterPagerView.register(UINib(nibName: "FilterTypeCell", bundle: nil),
+                                 forCellWithReuseIdentifier: "filterTypeCell")
+        filterPagerView.transformer = FSPagerViewTransformer(type: .linear)
+        filterPagerView.itemSize = imageSize
+
+        adjustPagerView.delegate = self
+        adjustPagerView.dataSource = self
+        adjustPagerView.register(UINib(nibName: "AdjustTypeCell", bundle: nil),
+                                 forCellWithReuseIdentifier: "adjustTypeCell")
+        adjustPagerView.itemSize = CGSize(width: 80, height: 80)
+    }
+
+    func configImagePickerView() {
+        var config = YPImagePickerConfiguration()
+        config.screens = [.library]
+        config.library.mediaType = .photo
+        imagePickerView = YPImagePicker(configuration: config)
+    }
+
+    func filterImage(_ input: CIImage, filter: String) -> UIImage? {
+        if filter != FilterType.original.identifier {
+            let filter = CIFilter(name: filter)
             filter?.setValue(input, forKey: kCIInputImageKey)
             guard let filteredImage = filter?.outputImage,
-                  let cgImage = context.createCGImage(filteredImage, from: filteredImage.extent)
+                  let cgImage = context.createCGImage(filteredImage,
+                                                      from: filteredImage.extent)
             else { return nil }
             return UIImage(cgImage: cgImage)
         }
@@ -127,22 +197,59 @@ class ViewController: UIViewController {
     }
 
     @IBAction func valueChanged(_ sender: CRRulerControl) {
-//        currentIntensity = sender.value / 100.0
+    }
+
+    @IBAction func didTapAddPhotoButton(_ sender: Any) {
+        imagePickerView.didFinishPicking { [unowned imagePickerView] items, _ in
+            if let photo = items.singlePhoto {
+                self.image = photo.image
+            }
+            imagePickerView.dismiss(animated: true, completion: nil)
+        }
+        present(imagePickerView, animated: true, completion: nil)
+    }
+
+    @IBAction func didTapEditTypeButton(_ sender: Any) {
     }
 }
 
 extension ViewController: FSPagerViewDelegate {
-    
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        if pagerView == filterPagerView {
+            currentFilter = listFilter[index].identifier
+        } else {
+            currentAdjust = listAdjust[index]
+        }
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
+    }
+
+    func pagerViewWillEndDragging(_ pagerView: FSPagerView, targetIndex: Int) {
+        if pagerView == filterPagerView {
+            currentFilter = listFilter[targetIndex].identifier
+        } else {
+            currentAdjust = listAdjust[targetIndex]
+        }
+    }
 }
 
 extension ViewController: FSPagerViewDataSource {
     func numberOfItems(in pagerView: FSPagerView) -> Int {
-        return listFilter.count
+        return pagerView == filterPagerView ? listFilter.count : listAdjust.count
     }
-    
+
     func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
-        <#code#>
+        if pagerView == filterPagerView {
+            let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "filterTypeCell", at: index) as! FilterTypeCell
+            let targetImage = image.resize(to: imageSize).toCIImage()
+            if let filteredImage = filterImage(targetImage, filter: listFilter[index].identifier) {
+                cell.contentImageView.image = filteredImage
+            }
+            return cell
+        } else {
+            let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "adjustTypeCell", at: index) as! AdjustTypeCell
+            cell.contentImageView.image = UIImage(named: listAdjust[index].image)
+            return cell
+        }
     }
-    
-    
 }
